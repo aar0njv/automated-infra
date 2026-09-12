35e64c59eb344623e598cb49edd6bcc6d6159533
Aaron Joy <aaronjoy382@gmail.com>
2026-09-12 23:57:37 +0530

    edit server.tf

--- module/web/server.tf
+++ module/web/server.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_key_pair" "tf-deployer" {
   key_name   = "ubuntu-ssh-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCoHz2UAYZGnVgXGC2XTUJRTOfR8OEEivHTkf405A+9/+yrgBPLsjGBbuyE5GTioSe7MH93kibCihMTpTY4nY06MYQ9MGFsjM7CMoy8LdGHY6BvizXYyjWE3VlJVwrL8xPbACrmwwqlBHqVNx1V7SIpZizEoO3ODnUK0r68BfBNx3bdzPtMeIvo9yqlhWpVMKpLmtZy/7P5oVzsEqNULQxfoBZOzYM7H6ncwsdOFBTMFohPAl73+8uS85VwMo2uQahqdNFHLeHHimJIlWp8dhutRcYqtf5hbOI7Gs+ujcRDiVh5l+KT7s3t0JI4F6NQ//XQPLpiJGYrHEUITiisrOeVJormSRGQ2URFR4yWidOwTPvM4ytC46bWeBz18GtP6lDW6sqekliG6LGpRPn+USYsPmh92tltZR15INJ3njkAkRYPmhgtDCtQ8lfu/i/EujvNZxMqmRf/0/WtkI0OUMjf8MBsAQGllUpsu/ZCG5uVao0/kzK15JVutaaxmdh0+216HhvhL9cVuHCPRDXHqLv7WYAEVrQolVQnuJriJXs9lzhJpKkJmkwQS/v9plLRj53JlUGXIPywC/vXlV9+xWoMqP845DAOy8DFV3Qv8D8X1CsLfS6SewxAx+b8P4sYe0olj2m7h5+4aYRWiaLLnmTsMEluaKqnfxycaQ1zHvsz4w== user@ubuntu"
+  public_key = "aws-pulic-key"
 }
 
 resource "aws_security_group" "tf-sg" {
